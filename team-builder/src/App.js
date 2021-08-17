11ce6b8e3772e91607f0b8ed8ca58636e7271d00
rdperry031 <rdperry031@gmail.com>
2021-08-17 17:38:17 -0500

    pushing to submit

---
+++ team-builder/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react'
+import Form from './Form'
+
+const initialValues = {
+  username: '',
+  email: '',
+  role: '',
+}
+
+
+function App() {
+  const [teamMember, setMember] = useState(initialValues)
+  
+  const updateForm = (inputName, inputValue) => {
+    setMember({...teamMember, [inputName]: inputValue})
+  }
+
+  const submitForm = () => {
+    const newMember = {
+      username: teamMember.username.trim(),
+      email: teamMember.email.trim(),
+      role: teamMember.role.trim(),      
+    }
+  if (!teamMember.username || !teamMember.email || !teamMember.role) return
+   
+  }
+  
+  return (
+    <div className="App">
+      <h1>FOORMS</h1>
+      <Form
+      values = {teamMember}
+      update = {updateForm}
+      submit = {submitForm}
+      />
+    </div>
+  );
+}
+
+export default App;
